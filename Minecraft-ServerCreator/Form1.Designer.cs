a000796240b1df8d20500a596d63f584f37e4e66
EagleDevs <58715617+Codyshep@users.noreply.github.com>
2024-02-25 14:59:02 -0600

    Add files via upload

---
+++ Minecraft-ServerCreator/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Minecraft_ServerCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.InstallContainer = new System.Windows.Forms.Panel();
+            this.RunServer = new System.Windows.Forms.Button();
+            this.setupserver = new System.Windows.Forms.Button();
+            this.InstallServer = new System.Windows.Forms.Button();
+            this.DirectoryButton = new System.Windows.Forms.Button();
+            this.selectedDirectoryTextBox = new System.Windows.Forms.TextBox();
+            this.outputListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.discordjoin = new System.Windows.Forms.Button();
+            this.InstallContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // InstallContainer
+            // 
+            this.InstallContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
+            this.InstallContainer.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.InstallContainer.Controls.Add(this.RunServer);
+            this.InstallContainer.Controls.Add(this.setupserver);
+            this.InstallContainer.Controls.Add(this.InstallServer);
+            this.InstallContainer.Controls.Add(this.DirectoryButton);
+            this.InstallContainer.Controls.Add(this.selectedDirectoryTextBox);
+            this.InstallContainer.Location = new System.Drawing.Point(3, 31);
+            this.InstallContainer.Name = "InstallContainer";
+            this.InstallContainer.Size = new System.Drawing.Size(327, 171);
+            this.InstallContainer.TabIndex = 0;
+            // 
+            // RunServer
+            // 
+            this.RunServer.Location = new System.Drawing.Point(216, 30);
+            this.RunServer.Name = "RunServer";
+            this.RunServer.Size = new System.Drawing.Size(86, 23);
+            this.RunServer.TabIndex = 5;
+            this.RunServer.Text = "Run Server";
+            this.RunServer.UseVisualStyleBackColor = true;
+            this.RunServer.Click += new System.EventHandler(this.RunServer_Click);
+            // 
+            // setupserver
+            // 
+            this.setupserver.Location = new System.Drawing.Point(122, 30);
+            this.setupserver.Name = "setupserver";
+            this.setupserver.Size = new System.Drawing.Size(88, 23);
+            this.setupserver.TabIndex = 4;
+            this.setupserver.Text = "Auto Setup";
+            this.setupserver.UseVisualStyleBackColor = true;
+            this.setupserver.Click += new System.EventHandler(this.SetupServer_Click);
+            // 
+            // InstallServer
+            // 
+            this.InstallServer.Cursor = System.Windows.Forms.Cursors.Default;
+            this.InstallServer.Location = new System.Drawing.Point(19, 30);
+            this.InstallServer.Name = "InstallServer";
+            this.InstallServer.Size = new System.Drawing.Size(97, 23);
+            this.InstallServer.TabIndex = 3;
+            this.InstallServer.Text = "Install Server";
+            this.InstallServer.UseVisualStyleBackColor = true;
+            this.InstallServer.Click += new System.EventHandler(this.InstallServer_Click);
+            // 
+            // DirectoryButton
+            // 
+            this.DirectoryButton.Font = new System.Drawing.Font("Microsoft YaHei", 6.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DirectoryButton.Location = new System.Drawing.Point(3, 141);
+            this.DirectoryButton.Name = "DirectoryButton";
+            this.DirectoryButton.Size = new System.Drawing.Size(79, 23);
+            this.DirectoryButton.TabIndex = 2;
+            this.DirectoryButton.Text = "Set Directory";
+            this.DirectoryButton.UseVisualStyleBackColor = true;
+            this.DirectoryButton.Click += new System.EventHandler(this.selectDirectoryButton_Click);
+            // 
+            // selectedDirectoryTextBox
+            // 
+            this.selectedDirectoryTextBox.Location = new System.Drawing.Point(88, 142);
+            this.selectedDirectoryTextBox.Name = "selectedDirectoryTextBox";
+            this.selectedDirectoryTextBox.Size = new System.Drawing.Size(232, 22);
+            this.selectedDirectoryTextBox.TabIndex = 1;
+            this.selectedDirectoryTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // outputListBox
+            // 
+            this.outputListBox.BackColor = System.Drawing.Color.White;
+            this.outputListBox.FormattingEnabled = true;
+            this.outputListBox.ItemHeight = 16;
+            this.outputListBox.Location = new System.Drawing.Point(336, 12);
+            this.outputListBox.Name = "outputListBox";
+            this.outputListBox.Size = new System.Drawing.Size(406, 340);
+            this.outputListBox.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Yu Gothic", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(80, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(165, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Vanilla Server Installer";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // discordjoin
+            // 
+            this.discordjoin.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.discordjoin.Location = new System.Drawing.Point(83, 323);
+            this.discordjoin.Name = "discordjoin";
+            this.discordjoin.Size = new System.Drawing.Size(155, 23);
+            this.discordjoin.TabIndex = 3;
+            this.discordjoin.Text = "Join Our Discord";
+            this.discordjoin.UseVisualStyleBackColor = true;
+            this.discordjoin.Click += new System.EventHandler(this.discordjoin_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(8)))), ((int)(((byte)(10)))), ((int)(((byte)(16)))));
+            this.ClientSize = new System.Drawing.Size(746, 358);
+            this.Controls.Add(this.discordjoin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.outputListBox);
+            this.Controls.Add(this.InstallContainer);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Opacity = 0.98D;
+            this.Text = "Minecraft Server Manager";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.InstallContainer.ResumeLayout(false);
+            this.InstallContainer.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel InstallContainer;
+        private System.Windows.Forms.TextBox selectedDirectoryTextBox;
+        private System.Windows.Forms.Button DirectoryButton;
+        private System.Windows.Forms.Button InstallServer;
+        private System.Windows.Forms.Button setupserver;
+        private System.Windows.Forms.Button RunServer;
+        private System.Windows.Forms.ListBox outputListBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button discordjoin;
+    }
+}
+
---
+++ Minecraft-ServerCreator/Form1.cs
@@ -0,0 +1,287 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace Minecraft_ServerCreator
+{
+    public partial class Form1 : Form
+    {
+        private string selectedDirectory;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void selectDirectoryButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowserDialog.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    selectedDirectory = folderBrowserDialog.SelectedPath;
+                    selectedDirectoryTextBox.Text = selectedDirectory;
+
+                    // Call UseSelectedDirectory method after selecting the directory
+                    UseSelectedDirectory();
+                }
+            }
+        }
+
+
+        // Example method using the selected directory
+        private void UseSelectedDirectory()
+        {
+            if (!string.IsNullOrEmpty(selectedDirectory))
+            {
+                // Use the selected directory variable here
+                AppendOutput("Selected directory: " + selectedDirectory);
+            }
+            else
+            {
+                AppendOutput("No directory selected.");
+            }
+        }
+
+        // Call UseSelectedDirectory method when the form loads
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            UseSelectedDirectory();
+        }
+
+        private void InstallServer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string powerShellScript = $@"
+                    $url='https://www.minecraft.net/en-us/download/server'
+                    $link=(Invoke-WebRequest -Uri $url).Links | Where-Object {{$_.innerText -like '*minecraft_server*.jar'}} | Select-Object -First 1 -ExpandProperty href
+                    Invoke-WebRequest -Uri $link -OutFile '{selectedDirectory}\minecraft_server_latest.jar'
+                ";
+
+                using (Process process = new Process())
+                {
+                    process.StartInfo = new ProcessStartInfo
+                    {
+                        FileName = "powershell.exe",
+                        Arguments = $"-ExecutionPolicy Bypass -Command \"{powerShellScript}\"",
+                        RedirectStandardOutput = true,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
+
+                    process.Start();
+                    AppendOutput("Installing Required Jar File, Wait A Second...");
+                    process.WaitForExit();
+
+                    AppendOutput(process.ExitCode == 0 ? "Minecraft server installed successfully! Please proceed to run Auto Setup." : "Installation Failed: Make sure you have a directory set and a stable internet connection!");
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendOutput($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void SetupServer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                // Construct the full path to minecraft_server_latest.jar
+                string jarFilePath = System.IO.Path.Combine(selectedDirectory, "minecraft_server_latest.jar");
+
+                if (System.IO.File.Exists(jarFilePath))
+                {
+                    // Prepare process start info
+                    // Prepare process start info
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        FileName = "java.exe",
+                        Arguments = $"-jar \"{jarFilePath}\"",
+                        WorkingDirectory = selectedDirectory, // Set the working directory
+                        UseShellExecute = false,
+                        RedirectStandardOutput = true,
+                        RedirectStandardError = true, // Redirect standard error
+                        CreateNoWindow = true
+                    };
+
+                    // Start the process
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo = startInfo;
+                        process.Start();
+
+                        // Read standard output and error streams asynchronously
+                        Task<string> outputTask = process.StandardOutput.ReadToEndAsync();
+                        Task<string> errorTask = process.StandardError.ReadToEndAsync();
+
+                        // Wait for both tasks to complete
+                        Task.WaitAll(outputTask, errorTask);
+
+                        // Check if any error occurred
+                        if (!string.IsNullOrEmpty(errorTask.Result))
+                        {
+                            AppendOutput($"An error occurred: {errorTask.Result}");
+                        }
+                        else
+                        {
+                            // Display standard output if needed
+                            string output = outputTask.Result;
+                            AppendOutput(output);
+                            Console.WriteLine(output);
+
+
+                            CreateTextFile(selectedDirectory, "eula.txt", "eula=true");
+                            AppendOutput("Manual EULA Bypassed ✅");
+                            AppendOutput("Jar File Executed Properly, You Can Now Start Your Server!");
+                        }
+
+                        process.WaitForExit();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("minecraft_server_latest.jar not found in the selected directory.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendOutput($"An error occurred: {ex.Message}");
+            }
+        }
+
+        static void CreateTextFile(string selectedDirectory, string filename, string content)
+        {
+            // Set the name of the file
+            string fileName = filename;
+
+            // Construct the full file path
+            string filePath = Path.Combine(selectedDirectory, fileName);
+
+            // Create a StreamWriter to write text to the file
+            using (StreamWriter writer = new StreamWriter(filePath))
+            {
+                // Write the content to the file
+                writer.Write(content);
+            }
+        }
+
+        private void RunServer_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Construct the full path to minecraft_server_latest.jar
+                string jarFilePath = Path.Combine(selectedDirectory, "minecraft_server_latest.jar");
+
+                if (File.Exists(jarFilePath))
+                {
+                    // Prepare process start info
+                    ProcessStartInfo startInfo = new ProcessStartInfo
+                    {
+                        FileName = "java.exe",
+                        Arguments = $"-jar \"{jarFilePath}\"",
+                        WorkingDirectory = selectedDirectory,
+                        UseShellExecute = false,
+                        CreateNoWindow = true
+                    };
+
+                    using (Process process = new Process())
+                    {
+                        process.StartInfo = startInfo;
+
+                        AppendOutput("Server Started!");
+                        process.Start();
+
+                        // Wait for the process to exit
+                        process.WaitForExit();
+
+                        AppendOutput("Server Stopped!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("minecraft_server_latest.jar not found in the selected directory.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            catch (Exception ex)
+            {
+                AppendOutput($"An error occurred: {ex.Message}");
+            }
+        }
+
+        // Method to append individual lines of output to the ListBox
+        private void AppendOutput(string output)
+        {
+            if (outputListBox.InvokeRequired)
+            {
+                outputListBox.Invoke(new Action<string>(AppendOutput), output);
+            }
+            else
+            {
+                // Split the output by newline characters
+                string[] lines = output.Split(new[] { '\r', '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                // Add each line as a separate item in the ListBox
+                foreach (string line in lines)
+                {
+                    outputListBox.Items.Add(line);
+                }
+            }
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void discordjoin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Open the Discord invite link in the default web browser
+                Process.Start("https://discord.gg/gtadev");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void InstallPlayItButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string downloadUrl = "https://github.com/playit-cloud/playit-agent/releases/download/v0.15.12/playit-windows-x86_64-signed.msi";
+                string downloadPath = Path.Combine(selectedDirectory, "playit-windows-x86_64-signed.msi");
+
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFile(downloadUrl, downloadPath);
+                }
+
+                // Once downloaded, you can start the installation process
+                Process.Start(downloadPath);
+            }
+            catch (Exception ex)
+            {
+                AppendOutput($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
